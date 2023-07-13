e400b3a130dd5a3176bcf660220d505142fc7093
hashalite <hashalite@gmail.com>
2023-07-13 15:25:42 -0400

    Port changes from fabric

--- src/main/java/net/xolt/freecam/mixin/LevelRendererMixin.java
+++ src/main/java/net/xolt/freecam/mixin/LevelRendererMixin.java
@@ -1,20 +1,12 @@
 package net.xolt.freecam.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.Camera;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderBuffers;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.phys.Vec3;
 import net.xolt.freecam.Freecam;
 import net.xolt.freecam.config.FreecamConfig;
-import org.joml.Matrix4f;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -24,20 +16,10 @@ import static net.xolt.freecam.Freecam.MC;
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
 
-    @Shadow
-    @Final
-    private RenderBuffers renderBuffers;
-
-    // Makes the player render if showPlayer is enabled.
-    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;checkPoseStack(Lcom/mojang/blaze3d/vertex/PoseStack;)V", ordinal = 0))
-    private void onRender(PoseStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightTexture lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
-        if (Freecam.isEnabled() && FreecamConfig.SHOW_PLAYER.get()) {
-            Vec3 cameraPos = camera.getPosition();
-            renderEntity(MC.player, cameraPos.x, cameraPos.y, cameraPos.z, tickDelta, matrices, renderBuffers.bufferSource());
+    @Inject(method = "renderEntity", at = @At("HEAD"), cancellable = true)
+    private void onRenderEntity(Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, PoseStack matrices, MultiBufferSource vertexConsumers, CallbackInfo ci) {
+        if (entity == MC.player && Freecam.isEnabled() && !FreecamConfig.SHOW_PLAYER.get()) {
+            ci.cancel();
         }
-    }
-
-    @Shadow
-    private void renderEntity(Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, PoseStack matrices, MultiBufferSource vertexConsumers) {
     }
 }
